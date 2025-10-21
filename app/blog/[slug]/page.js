073a591f768581f140c5eb10584aee7d5b367d35
Sanskar Gupta <sanskaargupta050@gmail.com>
2025-10-21 12:37:35 +0530

    4th commit

--- app/blog/[slug]/page.js
+++ app/blog/[slug]/page.js
@@ -1,3 +1,11 @@
+export async function generateStaticParams() {
+  return [
+    { slug: 'first-post' },
+    { slug: 'second-post' },
+    { slug: 'my-third-blog' },
+  ];
+}
+
 export default function BlogPost({ params }) {
 
   const { slug } = params;
