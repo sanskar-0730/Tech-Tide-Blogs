de6c29a57a0bfa4283f16abc25c1cd271bfcf9eb
Sanskar Gupta <sanskaargupta050@gmail.com>
2025-10-25 19:13:58 +0530

    changed ' to

--- app/not-found.js
+++ app/not-found.js
@@ -7,7 +7,7 @@ export default function NotFound() {
         <h1 className="notfound-title">404</h1>
         <h2 className="notfound-subtitle">Page Not Found</h2>
         <p className="notfound-text">
-          Oops! The page you're looking for doesn't exist or may have been moved.
+          Oops! The page you`re looking for doesn`t exist or may have been moved.
         </p>
         <Link href="/" className="notfound-button">
           Home Page
